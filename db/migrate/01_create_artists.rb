dda05b3568c195811ad991416f91cd396cce94a0
Christopher Leja <christopher.leja@gmail.com>
2020-02-27 18:23:07 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,10 +1,4 @@
-class CreateArtists < ActiveRecord::Migration[5.2]
-  def up
-  end
-  
-  def down
-  end
-  
+class CreateArtists < ActiveRecord::Migration[4.2]
   def change
     create_table :artists
     do |t|
