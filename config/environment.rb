7df88aed291de8e1ecf9d44d660323ae35030c38
Christopher Leja <christopher.leja@gmail.com>
2020-02-27 18:23:37 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -11,8 +11,7 @@ Bundler.require
 
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3"
-  :database => "db/artists.sqlite"
-  )
+  :database => "db/artists.sqlite")
 
 
 require_relative "../artist.rb"
